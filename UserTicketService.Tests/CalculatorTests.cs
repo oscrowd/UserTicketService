7405ee6792dfb8d90d6c3216aed7a342378821d0
oscrowd <oscrowd@gmail.com>
2023-06-10 15:42:24 +0300

    Добавьте файлы проекта.

---
+++ UserTicketService.Tests/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Assert = NUnit.Framework.Assert;
+
+namespace UserTicketService.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Fact]
+        public void MultiplicationMustReturnNotNullValue()
+        {
+            var calculator = new Calculator();
+            Assert.Equals(8, calculator.Multiplication(2, 4));
+        }
+        [Fact]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+        [Fact]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(200, 10) == 20);
+        }
+        [Test]
+        public void Division_MustThrowException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+        }
+    }
+}
---
+++ UserTicketService.Tests/TicketPriceTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserTicketService.Tests
+{
+    internal class TicketPriceTests
+    {
+        [Test]
+        public void TicketPriceMustReturnNotNullableTicket()
+        {
+            var mockTicketService = new Mock<ITicketService>();
+            mockTicketService.Setup(p => p.GetTicketPrice(1)).Returns(100);
+            mockTicketService.Setup(p => p.GetTicketPrice(2)).Returns(500);
+            mockTicketService.Setup(p => p.GetTicketPrice(3)).Returns(7800);
+
+            var ticketPriceTest = new TicketPrice(mockTicketService.Object);
+            Assert.That(ticketPriceTest.MakeTicketPrice(3) == 7800);
+        }
+        [Test]
+        public void GetTicketMustReturnNotNullableTicket()
+        {
+            var ticketServiceTest = new TicketService();
+            Assert.IsNotNull(ticketServiceTest.GetTicket(1));
+        }
+
+    }
+
+}
